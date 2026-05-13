06418a7404c33c6cd7da1d118669dd9084e9cc10
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2026-05-13 11:25:27 +0800

    Update py_to_exe.py

--- Python Script to Application/py_to_exe.py
+++ Python Script to Application/py_to_exe.py
@@ -1,5 +1,5 @@
 ### Python script with Qt GUI
 On Windows terminal:
-'''
+```
 python -m nuitka --standalone --show-progress --enable-plugin=pyside6 --include-qt-plugins=platforms,imageformats --noinclude-qt-plugins=qml,quick,charts,datavisualization,graphs --windows-console-mode=disable script_name.py
-'''
+```
